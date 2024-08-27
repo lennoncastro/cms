54fe83491f52ca6a84a04963388053a2f511da46
lennoncastro <lennon.castro.92@gmail.com>
2024-08-26 21:03:15 -0300

    feat: add title in app.dart

--- lib/src/configs/app.dart
+++ lib/src/configs/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'configs.dart';
 
+const String appName = 'Flutter CMS';
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: appName,
       routerConfig: Routes.routes,
     );
   }
