d3c8e76ba3f9fcd30c4f4f564709d7b272b8a49f
lennoncastro <lennon.castro.92@gmail.com>
2024-08-28 22:52:45 -0300

    feat: added improvements in bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,11 +1,13 @@
-for dir in cms_linter cms_network
+# shellcheck disable=SC2044
+for dir in $(find ./ -type d -name "cms_*" -not -path "*/.dart_tool/build/generated/*");
 do
+  echo $dir
   cd $dir
   rm -rf *.lock
   flutter pub get
+  flutter pub run build_runner build --delete-conflicting-outputs
   cd ..
 done
-echo $(pwd)
 rm -rf *.lock
 flutter pub get
 flutter pub run build_runner build --delete-conflicting-outputs
